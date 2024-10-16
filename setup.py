7e101c520558649ac63914d5641e120ce07d306d
Iacob Casian <44318678+casiacob@users.noreply.github.com>
2024-10-16 17:44:22 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     author="",
     author_email="",
     description="",
-    install_requires=["jax", "matplotlib", "pandas", "jaxlib==0.4.20+cuda12.cudnn89 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html"],
+    install_requires=["jax", "matplotlib", "pandas"],
     zip_safe=False,
 )
