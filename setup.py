f8d9ddde91cfea1fa60af58b3c224d73a20a23f2
Iacob Casian <44318678+casiacob@users.noreply.github.com>
2024-10-17 10:10:59 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name="noc",
     version="0.0.1",
-    author="",
-    author_email="",
+    author="Casian Iacob",
+    author_email="casian.iacob@aalto.fi",
     description="",
     install_requires=["jax[cuda12]", "matplotlib", "pandas"],
     zip_safe=False,
